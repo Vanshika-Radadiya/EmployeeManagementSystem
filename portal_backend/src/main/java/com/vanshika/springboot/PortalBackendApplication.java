4d968496058c1230dffc6762d2b4dd38dbffd931
Vanshika-Radadiya <131805356+Vanshika-Radadiya@users.noreply.github.com>
2023-12-04 09:42:04 +0530

    Uploaded  Backend files

---
+++ portal_backend/src/main/java/com/vanshika/springboot/PortalBackendApplication.java
@@ -0,0 +1,14 @@
+package com.vanshika.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+//@CrossOrigin(origins = "http://localhost:3000")
+@SpringBootApplication
+public class PortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalBackendApplication.class, args);
+	}
+
+}
